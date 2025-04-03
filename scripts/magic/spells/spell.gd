4eadb4fcfee610a4499bd2ca480880487cf7ed4e
Federico Alejo Marquez <17768182+fefu-marquez@users.noreply.github.com>
2025-04-03 19:23:43 -0300

    style: remove comented code

--- scripts/magic/spells/spell.gd
+++ scripts/magic/spells/spell.gd
@@ -4,14 +4,6 @@ extends Node
 
 @export var MAGIC_COST: float
 
-### This creates the specific child of the Spell class, instead of the generic
-### Spell.
-#static func New(args: Dictionary):
-	#return load(args['path_to_script']).new(args)
-#
-#func _init(args: Dictionary):
-	#magic_cost = args['magic_cost']
-
 ## This methods has the logic of what the spell does.
 ## It returns true if the casting was successful, false otherwise.
 func cast(target=null) -> bool:
